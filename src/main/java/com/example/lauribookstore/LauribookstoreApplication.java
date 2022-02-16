3336f4c56faac07233511d2eb3751d0ed4e85027
LriKrj <lauri.karjalainen3@hotmail.com>
2022-02-16 14:08:29 +0200

    second commit

--- src/main/java/com/example/lauribookstore/LauribookstoreApplication.java
+++ src/main/java/com/example/lauribookstore/LauribookstoreApplication.java
@@ -1,13 +1,37 @@
 package com.example.lauribookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.lauribookstore.domain.Book;
+import com.example.lauribookstore.domain.BookRepository;
+
+
 
 @SpringBootApplication
 public class LauribookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(LauribookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(LauribookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple books");
+			repository.save(new Book("Harry Potter", "JK Rowling", 2005, "951-98548-9-4", 20.20));
+			repository.save(new Book("Lord of the rings", "Tolkien", 1998, "951-98548-9-1", 30.50));
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
 	}
 
 }
--- src/main/java/com/example/lauribookstore/web/BookController.java
+++ src/main/java/com/example/lauribookstore/web/BookController.java
@@ -1,12 +1,48 @@
 package com.example.lauribookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.lauribookstore.domain.Book;
+import com.example.lauribookstore.domain.BookRepository;
+
+
+
+
+
+
 
 @Controller
 public class BookController {
-	@RequestMapping("/index")
-	public String viewBooks() {
-		return "";
+	@Autowired
+	private BookRepository repository; 
+	@GetMapping("/booklist")
+	public String viewBooks(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
-}
+	@RequestMapping("/create")
+    public String addBook(Model model){
+    	model.addAttribute("books", new Book());
+        return "addbook";
+    }
+	
+	@PostMapping("/save")
+	public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }    
+	
+	@GetMapping("/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../studentlist";
+    }     
+	}
+	
+	
+
