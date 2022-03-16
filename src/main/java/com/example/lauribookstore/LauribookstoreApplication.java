c8466d4fdc5dc6a6a616c9b7ab0bde11eec96e90
LriKrj <lauri.karjalainen3@hotmail.com>
2022-03-16 17:37:56 +0200

    chapter 6 tehty

--- src/main/java/com/example/lauribookstore/LauribookstoreApplication.java
+++ src/main/java/com/example/lauribookstore/LauribookstoreApplication.java
@@ -11,6 +11,8 @@ import com.example.lauribookstore.domain.Book;
 import com.example.lauribookstore.domain.BookRepository;
 import com.example.lauribookstore.domain.Category;
 import com.example.lauribookstore.domain.CategoryRepository;
+import com.example.lauribookstore.domain.User;
+import com.example.lauribookstore.domain.UserRepository;
 
 
 
@@ -23,7 +25,7 @@ public class LauribookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple books");
 			
@@ -34,7 +36,13 @@ public class LauribookstoreApplication {
 			repository.save(new Book("Harry Potter", "JK Rowling", 2005, "951-98548-9-4", 20.20, crepository.findByName("Fantasy").get(0)));
 			repository.save(new Book("Lord of the rings", "Tolkien", 1998, "951-98548-9-1", 30.50, crepository.findByName("Fiction").get(0)));
 			
-			
+			// Create users: admin/admin user/user
+			User user1 = new User("user",
+			"$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin",
+			"$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			
 			log.info("fetch all books");
--- src/main/java/com/example/lauribookstore/web/BookController.java
+++ src/main/java/com/example/lauribookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -24,44 +25,54 @@ public class BookController {
 
 	@Autowired
 	private CategoryRepository crepository;
-	
-	
-	//show books
+
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+
+	// show books
 	@RequestMapping(value = { "/", "/booklist" })
 	public String viewBooks(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
-	//rest show books
+
+	// rest show books
 	@RequestMapping(value = "/books", method = RequestMethod.GET)
 	public @ResponseBody List<Book> bookListRest() {
 		return (List<Book>) repository.findAll();
 	}
-	//rest search by id
+
+	// rest search by id
 	@RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
 	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
 		return repository.findById(bookId);
 	}
-	//add book
+
+	// add book
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		model.addAttribute("categories", crepository.findAll());
 		return "addbook";
 	}
-	
+
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
 	}
-	//delete book
+
+	// delete book
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
-	//edit book
+
+	// edit book
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
